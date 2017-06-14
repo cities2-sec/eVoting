31e2364906981400f3338c188c3b70e45f997ff2
azeez <az6bcn@gmail.com>
2017-06-14 17:36:59 +0200

    cambios pailler urna v1

---
+++ web/scripts/paillier-big-integer.js
@@ -0,0 +1,67 @@
+
+
+function lcm(a,b) {
+    this.p = bigInt(a);
+    this.q = bigInt(b);
+    return this.p.multiply(this.q).divide(this.p.gcd(this.q));
+}
+
+var paillier = {
+    publicKey: function(bits, n) {
+        this.bits = bits;
+        this.n = n;
+        this.n2 = this.n.pow(2);
+        this.beta = bigInt.rand(this.n);
+        this.alfa = bigInt.rand(this.n);
+        this.g = this.alfa.multiply(this.n).add(1).mod(this.n2).multiply(this.beta.modPow(this.n,this.n2)).mod(this.n2);
+    },
+    privateKey: function(lambda,pubkey) {
+        this.lambda = lambda;
+        this.pubkey = pubkey;
+        this.mu = this.pubkey.g.modPow(this.lambda,this.pubkey.n2).subtract(1).divide(this.pubkey.n);
+        this.u = bigInt(this.mu).modInv(pubkey.n);
+
+    },
+    generateKeys: function(modulusbits) {
+        var p, q, n,random, keys = {};
+        this.bitlength = modulusbits || 2048;
+        console.log("Generando PAILLIER Keys Elecciones de", this.bitlength, "bits");
+        p = bigInt.prime(this.bitlength / 2);
+        do {
+            q = bigInt.prime(this.bitlength / 2);
+        } while (q.compare(p) === 0 && q.gcd(p)===1);
+
+        n = p.multiply(q);
+        keys.publicKey = new paillier.publicKey(this.bitlength,n);
+
+        //λ = lcm(p − 1, q − 1)
+        lambda = lcm(p.subtract(1),q.subtract(1));
+        keys.privateKey = new paillier.privateKey(lambda,keys.publicKey);
+
+        return keys;
+    }
+};
+
+paillier.publicKey.prototype = {
+    //c = g^m · r^n mod n^2
+    encrypt: function(m,r) {
+        // return  this.g.powm(m,this.n2).mul(r.powm(this.n,this.n2)).mod(this.n2);
+    }
+};
+
+paillier.privateKey.prototype = {
+    encrypt: function(m) {
+        return m.modPow(this.publicKey.e, this.publicKey.n);
+    },
+    verify: function(c) {
+        return c.modPow(this.publicKey.e, this.publicKey.n);
+    },
+    sign: function(m) {
+        return m.modPow(this.d, this.publicKey.n);
+    },
+    decrypt: function(c) {
+        console.log("desencriptando...");
+        return c.modPow(this.lambda,this.pubkey.n2).subtract(1).divide(this.pubkey.n).mod(this.pubkey.n).multiply(this.u).mod(this.pubkey.n);
+    }
+};
+//module.exports = paillier;
--- web/views/urna/urna.js
+++ web/views/urna/urna.js
@@ -10,6 +10,49 @@ angular.module('MainApp', ['ngStorage'])
 
 	$scope.election = {};
 	$scope.parties = {};
+    $scope.tuvoto = {};
+    var Krsa = rsa.generateKeys(512);
+
+    //encriptarPaillier
+    function encryptPubkeyPaillier(m,r,n,g){
+        var n2 = bigInt(n).pow(2);
+        return (bigInt(g).modPow(m,n2).mod(n2)).multiply(r.modPow(n,n2)).mod(n2);
+    }
+
+    function blindMsg (message,random,e,n){
+        return  (message.multiply(random.modPow(e,n))).mod(n);
+    }
+    function decryptPubKeyRemote (signed,e,n){
+        return signed.modPow(e,n);
+    }
+
+    function convertToHex(str) {
+        var hex = '';
+        for (var i = 0; i < str.length; i++) {
+            hex += '' + str.charCodeAt(i).toString(16);
+        }
+        return hex;
+    }
+    function hexToAscii(hexx) {
+        var hex = hexx.toString();//force conversion
+        var str = '';
+        for (var i = 0; i < hex.length; i += 2)
+            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+        return str;
+    }
+    function hexToBase64(str) {
+        return btoa(String.fromCharCode.apply(null,
+            str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" "))
+        );
+    }
+    function base64ToHex(str) {
+        for (var i = 0, bin = atob(str.replace(/[\r\n]+$/, "")), hex = []; i < bin.length; ++i) {
+            var tmp = bin.charCodeAt(i).toString(16);
+            if (tmp.length === 1) tmp = "0" + tmp;
+            hex[hex.length] = tmp;
+        }
+        return hex.join(" ");
+    }
 
 
 	$(function () {
@@ -113,46 +156,100 @@ angular.module('MainApp', ['ngStorage'])
     };
 
     $scope.voteConfirm = function() {
+        //peticion para clavePaillier
 
-		//encriptar con la publica de la mesa antes de enviarlo a la BD
+        $http.get('/urna/kpaillier').then(function (response) {
+
+            //console.log(response);
+            //console.log(response.data[0].publicKey.n);
+
+            var msgHEX = convertToHex($scope.idvotedParty); //voto por el numero de id del candidato
+            var msgToInt = bigInt(msgHEX, 16);
+            var n = response.data[0].publicKey.n;
+            var g = response.data[0].publicKey.g;
+            var rand = bigInt.randBetween(0,n);
 
 
-        //firmar el voto encriptado con la Privada del usuario
+            if ($scope.idvotedParty.id == '01') {var voto = '001';}
+            if ($scope.idvotedParty.id == '02') {var voto = '010'; }
+            if ($scope.idvotedParty.id == '03') {var voto = '100'; }
+
+            //encriptar el voto con la publica de la mesa usando paillier
+            var Pencriptado = encryptPubkeyPaillier(msgToInt,rand,n,g);
+            var VotoEncryptado = Pencriptado.toString(16); //voto que el votante mismo encripta
+
+            //firmar voto encriptado con RSA con cLAVE PRIVADA
+            var VotoEncriptadoFirmar = Krsa.VotoEncryptado.modPow(this.d, this.publicKey.n);; //m^d x r mod n
+            console.log('Votoencriptado firmado: '+ VotoEncriptadoFirmar.toString(16));
+            console.log("RSAKes: " + Krsa.publicKey.n.toString());
+            tuvoto = VotoEncryptado;
+
+            console.log("tuvoto " + tuvoto );
+
+        }, function (err) {
+            console.log("error al obtener las Claves de Paillier" + err.data);
+        });
+
+
 
 
         //concat
-		var voto = $scope.idvotedParty + '%' + $scope.file.id + '%' + $scope.file.pk;
-		console.log("votoooo");
-		console.log(voto);
+        var voto = $scope.idvotedParty + '%' + $scope.file.id + '%' + $scope.file.pk;
+        console.log("votoooo");
+        console.log(voto);
 
-		var voto2 = {"voto" : voto};
-		console.log(voto2);
-		//send to BD
-        $http.post('/urna/vote',{voto: voto})
-            .then(function successCallback(response){
-                if(response.status == 200){
-                    console.log(response.status+ " " +response.data.message);
+        //send to BD
+        $http.post('/urna/vote', {voto: voto})
+            .then(function successCallback(response) {
+                if (response.status == 200) {
+                    console.log(response.status + " " + response.data.message);
 
                 }
-            },function errorCallback(response){
-                if(response.status == 500){
+            }, function errorCallback(response) {
+                if (response.status == 500) {
                     console.log(response.data.message);
                 }
-                if(response.status == 400){
+                if (response.status == 400) {
                     console.log('Error: ' + response.data.message);
                 }
-                if(response.status == 500){
+                if (response.status == 500) {
                     console.log('Error: ' + response.data.message);
                 }
-                if(response.status == 403){
+                if (response.status == 403) {
                     console.log('Error: ' + response.data.message);
 
 
                 }
-                if(response.status == 404){
+                if (response.status == 404) {
                     console.log('Error: ' + response.data.message);
                 }
             })
+
 	};
+});
+
+/*
+    $scope.EncriptarVoto = function () {
+        //aqui conectarermos con la entidad de elecciones para conocer la Clave publica (paillier)
+        $http.get('http://localhost:3000/Elecciones/KElecciones').success(function (response) {
+
+            $scope.showMevotoencriptado = !$scope.showMevotoencriptado;
+
+            // var msgHEX = convertToHex($scope.candidato.id); //voto por el numero de id del candidato
+
+
+
+            var msgHEX = convertToHex(voto);
+            var msgToInt = bigInt(msgHEX,16);
+            var n = response.EpublicKEY.n;
+            var g = response.EpublicKEY.g;
+            var rand = bigInt.randBetween(0,n);
+            var Cencriptado = encryptPubkeyPaillier(msgToInt,rand,n,g);
+            var VotoEncryptado = Cencriptado.toString(16); //voto que el votante mismo encripta
+            $scope.tuvoto = VotoEncryptado;
+            //$scope.tuvoto = Cencriptado;
+        });
+    };
 
 });
+ */
